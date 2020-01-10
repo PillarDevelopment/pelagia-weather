8130002ef17485757bec6151e7c8e50f8683cfba
pillardev <2622610@gmail.com>
2020-01-10 12:17:52 +0300

    added runtime

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,31 +1,27 @@
-import telebot
 import pyowm
+from colorama import Fore, Back
 
 owm = pyowm.OWM('6d00d1d4e704068d70191bad2673e0cc')
-bot = telebot.TeleBot("953646504:AAGGcusriWInq-JQlNIbRdfVZE6wxBAq8PE")
+print( Back.GREEN)
+print( Fore.BLACK)
 
-# @bot.message_handler(commands=['start', 'help'])
-# def send_welcome(message):
-#	bot.reply_to(message, "Howdy Ho, how are you doing?")
+place = input("Change your city: ")
 
-@bot.message_handler(content_types=['text'])
-def send_echo(message):
-    observation = owm.weather_at_place(message.text)
-    w = observation.get_weather()
-    temp = w.get_temperature('celsius')["temp"]
+observation = owm.weather_at_place(place)
+w = observation.get_weather()
+temp = w.get_temperature('celsius')["temp"]
 
-    answer = " In the city " + message.text + " now " + w.get_detailed_status() + "\n"
-    answer += "Temperature now " + str(temp) + "\n\n"
-    if temp < 10:
-        answer += "The coldest"
-    elif temp < 20:
-        answer += "Very cold"
-    else:
-        answer += "Normally"
+print( Back.CYAN )
+print( Fore.BLACK)
+print(" In the city " + place + " now " + w.get_detailed_status())
 
-    bot.send_message(message.chat.id, answer)
+print( Back.BLUE )
+print( Fore.BLACK)
+print( "Temperature now " + str(temp) )
 
-try:
-    bot.polling(none_stop=True, interval=0)
-except Exception:
-    pass
+if temp < 10:
+    print( "The coldest" )
+elif temp < 20:
+    print( "Very cold" )
+else:
+    print( "Normally" )
